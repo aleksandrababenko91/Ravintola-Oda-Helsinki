b29b91ec2b205baa30db0c4678d759c43e018e37
aleksandrababenko91 <127403692+aleksandrababenko91@users.noreply.github.com>
2024-07-21 18:52:27 +0100

    added homepage nmae for deploy

--- src/components/shared/Footer/SocialLinks/SocialLinks.jsx
+++ src/components/shared/Footer/SocialLinks/SocialLinks.jsx
@@ -7,7 +7,7 @@ export default function SocialIcons ({item}) {
     <div className={styles.links}>
       <a href={url} target="_blank" rel="noopener noreferrer">
         <Image 
-          alt={name}
+          alt="image"
           src={image}
           width={50}
           height={50}
